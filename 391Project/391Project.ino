54c54b6885ea4d52368370b5e5c511831fc3efb3
bbwanaz <bbwanakocha@usapglobal.org>
2020-03-04 18:14:09 -0800

    new

--- 391Project/391Project.ino
+++ 391Project/391Project.ino
@@ -1,3 +1,4 @@
+#include <Stepper.h>
 int windCount, Direction;
 int prevState [2];
 int currState[2];
@@ -7,6 +8,11 @@ double angle,angle_1;
 
 int sens1prev, sens1time, sens1prevtime;
 int sens2prev,sens2time, sens2prevtime;
+int stepCount = 0;
+const int stepsPerRevolution = 200;
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+int pinA = 2;
+int pinB = 3;
 
 
 /*Author: Brandon Bwanakocha
@@ -60,29 +66,9 @@ if((prev[0] == 0 && prev[1] == 1) && (curr[0] == 0 && curr[1] == 0))
  return 2;
   }
 
-
-
-
-void setup() {
-// start with previous state given by the thing.
-
-  prevState[0] = analogRead(A5)*volts > 4.0 ? HIGH: LOW; 
-  prevState[1] = analogRead(A4)*volts > 4.0 ? HIGH: LOW;
-
-  pinMode(LED, OUTPUT);
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  Serial.begin(9600);
-  windCount = 0;
-  Direction = 2;
-
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  currState[0] = analogRead(A5)*volts> 4.0 ? HIGH: LOW;
-  currState[1] = analogRead(A4)*volts> 4.0 ? HIGH: LOW;
+  void ISR_(){
+    currState[0] = digitalRead(pinA);
+  currState[1] = digitalRead(pinB);
 
 
     
@@ -92,16 +78,43 @@ void loop() {
     }
     prevState[0] = currState[0];
     prevState[1] = currState[1];
-    
-
-
-    
-  
-
  Serial.print(angle);
  Serial.print("     ");
  Serial.print(Direction);
  Serial.print("\n");
+    }
+
+
+
+
+void setup() {
+// start with previous state given by the thing.
+
+
+  pinMode(pinA,INPUT);
+  pinMode(pinB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(pinA),ISR_,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pinB),ISR_,CHANGE);
+  prevState[0] = digitalRead(pinA);
+  prevState[1] = digitalRead(pinB);
+  Serial.begin(9600);
+  windCount = 0;
+  Direction = 2;
+
+interrupts();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+    
+
+
+  myStepper.setSpeed(100);
+  myStepper.step(2);  
+  
+
+
 
 
 }
